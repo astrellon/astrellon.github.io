7c9e59153b6736c2948bea1b2f5cdd6162930638
astrellon <sovereign250@gmail.com>
2025-12-01 17:46:39 +1100

    Made title consistent

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -7,7 +7,7 @@ export const AboutMe: PostState[] = [
             {
                 "text": [
                     "Software developer from Melbourne Australia.",
-                    "I'm currently working at [link](Fluent Cargo | https://www.fluentcargo.com/) as a principal software engineer. Primary on data processing and software architecture.",
+                    "I'm currently working at [link](Fluent Cargo | https://www.fluentcargo.com/) as a principal software developer. Primary on data processing and software architecture.",
                     "I've previously worked at [link](migenius | https://www.migenius.com/), [link](Rome2rio | https://rome2rio.com) and [link](Metro Trains Melbourne | https://metrotrains.com.au).",
                     "You can contact me via [link](email | mailto:alan.lawrey@gmail.com) or find me on [link](Bluesky | https://bsky.app/profile/astrellon.bsky.social), [link](LinkedIn | https://www.linkedin.com/in/alan-lawrey-451431379/) or [link](reddit | https://www.reddit.com/user/astrellon3/).",
                     "These are the things I enjoy working on:",
